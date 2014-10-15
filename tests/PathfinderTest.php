efbb418f4cc95a365ab60c5b7e41e445c996241b
Djane Rey Mabelin <zen.mabelin@syntacticsinc.com>
2014-10-15 17:30:05 +0800

    simple test

--- tests/PathfinderTest.php
+++ tests/PathfinderTest.php
@@ -1,6 +1,6 @@
 <?php
 use Djaney\Pathfinder\Pathfinder;
-use Djaney\Pathfinder\Route;
+use Djaney\Pathfinder\Transit;
 use Djaney\Pathfinder\Node;
 use Djaney\Pathfinder\Segment;
 use Djaney\Pathfinder\Path;
@@ -8,21 +8,28 @@ class PathfinderTest extends TestCase {
 
 
 
-	public function testSimpleOnePath()
+	public function testOneRouteOneSegment()
 	{
 		$p = new Pathfinder;
-		$r->addRoute('Jeep 1');
-		$n1 = $r->addNode('Station 1');
-		$n2 = $r->addNode('Station 2');
-		$r->connectNodes($n1,$n2,array('distance'=>1,'fare'=>2));
+
+		$n1 = $p->addNode('n1','Station 1');
+		$n2 = $p->addNode('n2','Station 2');
+
+		$t = $p->addTransit('r1','Jeep 1');
+		$t->addSegment($n1,$n2,array('distance'=>1));
 
 		$pa = $p->findPath($n1,$n2,'distance');
 
-		
+		// first step
+		$this->assertEquals($pa->steps[0]->node->name,'Station 1');
+		$this->assertEquals($pa->steps[0]->route->name,'Jeep 1');
+		$this->assertEquals($pa->steps[0]->segment->id,'r1-n1-n2');
 
-		$this->assertEquals($pa->nodes[0]->name,'Station 1');
-		$this->assertEquals($pa->nodes[2]->name,'Station 2');
+		// second step
+		$this->assertEquals($pa->steps[2]->node->name,'Station 2');
+		$this->assertEquals($pa->steps[2]->route->name,'Jeep 1');
+		$this->assertEquals($pa->steps[2]->segment->id,NULL);
+
 		$this->assertEquals($pa->metricTotal('distance'),1);
-		$this->assertEquals($pa->metricTotal('fare'),2);
 	}
 }
